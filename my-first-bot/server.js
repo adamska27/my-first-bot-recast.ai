29e676784f651701200e4983628dee8367c7f61f
Adamska <adams.roy27@gmail.com>
2017-09-06 12:06:38 +0200

    clean my folder work

--- my-first-bot/server.js
+++ my-first-bot/server.js
@@ -1,49 +1,50 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-
-const app = express();
-const port = process.env.PORT || 5000;
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const recastai = require('recastai')
 
 /*--------MIDDLEWARE----------*/
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, HEAD, PATCH');
-  res.header('Access-Control-Allow-Headers', 'Origin, Access-Control-Allow-Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Max-Age', '3600');
-  next();
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, HEAD, PATCH')
+  res.header('Access-Control-Allow-Headers', 'Origin, Access-Control-Allow-Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Max-Age', '3600')
+  next()
 });
 
-app.use(morgan(':method : url :status :res[content-length] - :response-time ms'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan(':method : url :status :res[content-length] - :response-time ms'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
-const recastai = require('recastai');
-const client = new recastai.request('2acb39742532466c668899188d2d7b13', 'en');
+var token
+var client
 
-const test = (message) => {
-  client.converseText(message)
-  .then( (res) => {
-  if (res.action) console.log('Action: ', res.action.slug)
-  const reply = res.reply()
-  console.log('Reply: ', reply)
+app.get('/', (req, res) => {
+  res.send('Welcome to my app!')
+  console.log('token from the home: ', token)
+  console.log('client from the home: ', client)
   })
-  return 'hello'
-}
+  .post('/createToken', (req, res) => {
+    token = req.body.token
+    console.log('token: ', token)
+    client = new recastai.request(token, 'en')
+    console.log('client: ', client)
+    res.send('The token is in the place')
+  })
+  .post('/', (req, res) => {
+  const input = req.body.userMessage.join()
+  console.log('input: ', input)
+  client.converseText(input)
+  .then(result => {
+    const botReply = result.reply()
+    console.log('botReply: ', botReply)
+    return botReply
+  })
+  .then( result => {
+    console.log('result: ', result)
+    res.send(result)
+    })
+  })
 
-app.get('/get', (req, res) => {
-  res.send('hello world')
-})
-
-.post('/post', (req, res) => {
-  console.log(`you've post something`);
-
-  const message = req.body.message
-
-  console.log('message ', message);
-
-  test(message).then( () => console.log('hello'))
-
-})
-
-app.listen(port, () => console.log(`The app is listenning on the port ${port}`))
+app.listen(3001, () => console.log('App is running on port 3001'))
